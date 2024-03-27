76f28f66db56763b038c491c64f26548959d76fd
ksungjoon <gkxm0443@naver.com>
2024-03-27 18:03:12 +0900

    fix: 경험사항 수정

--- src/pages/ExperiencesPage.jsx
+++ src/pages/ExperiencesPage.jsx
@@ -58,7 +58,10 @@ const ExperiencesPage = () => {
                             <span className="Date">2023년 9월 17일</span>
                             <h3 className="TimelineTitle">2023 삼성 청년 SW 아카데미 특화 프로젝트 우수상</h3>
                             <p className="TimelineContents">
-                            신한은행과 SSAFY에서 주관하는 해커톤 대회에 참여한 경험이 있습니다.
+                            [2학기] 특화 프로젝트 광주 2반 1등
+                            </p>
+                            <p className="TimelineContents">
+                            삼성 청년  SW 아카데미 특화 프로젝트 우수팀으로 선정 되었습니다.
                             </p>
                         </div>
                         </li>
@@ -68,7 +71,8 @@ const ExperiencesPage = () => {
                             <span className="Date">2023년 9월 17일</span>
                             <h3 className="TimelineTitle">신한 해커톤 프로젝트 우수상</h3>
                             <p className="TimelineContents">
-                            삼성 , 고용노동부 주관 SW 역량 향상 교육 프로그램
+                            [2023년 신한은행 해커톤 with SSAFY 우수상]
+                            모아일기 웹앱 에플리케이션 개발로 우수상(3등) 수상
                             </p>
                         </div>
                         </li>
@@ -78,7 +82,10 @@ const ExperiencesPage = () => {
                             <span className="Date">2023년 9월 17일</span>
                             <h3 className="TimelineTitle">2023 삼성 청년 SW 아카데미 공통 프로젝트 우수상</h3>
                             <p className="TimelineContents">
-                            조선대학교에서 학점 3.82로 우수한 성적으로 졸업을 하였습니다.
+                            [2학기] 공통 프로젝트 광주 2반 1등
+                            </p>
+                            <p className="TimelineContents">
+                            삼성 청년  SW 아카데미 공통 프로젝트 우수팀으로 선정 되었습니다.
                             </p>
                         </div>
                         </li>
@@ -92,7 +99,7 @@ const ExperiencesPage = () => {
                             <span className="Date">2023년 9월 17일</span>
                             <h3 className="TimelineTitle">데이터분석[준전문가](ADsP)</h3>
                             <p className="TimelineContents">
-                            신한은행과 SSAFY에서 주관하는 해커톤 대회에 참여한 경험이 있습니다.
+                            데이터 이해에 대한 기본지식을 바탕으로 데이터분석 기획 및 데이터분석 등의 직무 수행 역량
                             </p>
                         </div>
                         </li>
